b05931f105a5ba4c64f2b014988cc681cb5bcd39
Sarouar Hossain <sarouarhossain@Lenovo-ThinkBook>
2020-05-07 02:39:54 +0900

    test api added

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,19 @@
+package com.example.todo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+    @GetMapping("/test/{name}")
+    public String test(@PathVariable String name){
+        String x = "Hello ";
+        return x+name;
+    }
+}
